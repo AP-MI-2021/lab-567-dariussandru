b1c3a8517d5c1d29e4cbf1a909ef82716828ce89
dariussandru <91565295+dariussandru@users.noreply.github.com>
2021-10-27 03:03:35 +0300

    Delete testAll.py

--- Tests/testAll.py
+++
@@ -1,8 +0,0 @@
-from Tests.test_Crud import testAdaugaCheltuieli, testStergereCheltuieli
-from Tests.test_Domain import testCheltuieli
-
-
-def runAllTests():
-    testCheltuieli()
-    testAdaugaCheltuieli()
-    testStergereCheltuieli()
